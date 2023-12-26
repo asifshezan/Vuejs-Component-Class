07988d5aa23de2bb1e786dd4f4eecd6e47d803c5
asifshezan <asifshezan7@gmail.com>
2023-12-27 01:40:55 +0600

    How easily use component.

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import tagLine from './components/tagLine.vue';
+import hero from './components/hero.vue';
+import videoButton from './components/videoButton.vue';
+import logo from './/components/logo.vue';
+</script>
+
+<template>
+<section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+      <tagLine/>
+        <hero/>
+        <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+          <videoButton/>
+        </div>
+        <logo/>
+    </div>
+</section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/logo.vue
@@ -0,0 +1,21 @@
+<script setup>
+import logo1st from './logo1st.vue';
+import logo2nd from './logo2nd.vue';
+import logo3rd from './logo3rd.vue';
+</script>
+
+<template>
+<div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+            <span class="font-semibold text-gray-400 uppercase">FEATURED IN</span>
+            <div class="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
+                <logo1st/>
+                <logo2nd/>
+                <logo3rd/>
+                  
+            </div>
+        </div> 
+</template>
+
+<style scoped>
+
+</style>
